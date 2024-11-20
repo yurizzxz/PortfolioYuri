ed3ca66e0e6ca60ab9ef77322867ff4606210183
yurizzxz <yuri.alves.silva@gmail.com>
2024-11-20 17:29:20 -0300

    Layout: Iniciando a seção Services do projeto

--- src/app/_components/content.tsx
+++ src/app/_components/content.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const Content = () => {
     return (
         <>
-        
+            <main className="services-section">
+                <section className="services">
+
+                </section>
+            </main>
         </>
     )
 }
