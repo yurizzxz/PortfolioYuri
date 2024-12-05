cd2f4216736ea6760bef51095bd689fac81ae3d2
yurizzxz <yuri.alves.silva@gmail.com>
2024-12-05 20:51:11 -0300

    Integração com firebase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN],
+  },
 };
 
 export default nextConfig;
--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -1,50 +1,47 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import dbd from "../_assets/img/daybyday.png";
 import { motion } from "framer-motion";
+import { db } from "../firebaseconfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-const projects = [
-  {
-    id: 1,
-    title: "DayByDay",
-    description: "DayByDay",
-    image: dbd,
-    link: "https://github.com/yurizzxz/tcc-daybyday",
-    filter: "Front-End Backend",
-  },
-  {
-    id: 2,
-    title: "FitFusion",
-    description: "FitFusion",
-    image: dbd,
-    link: "https://github.com/yurizzxz/tcc-daybyday",
-    filter: "Front-End Backend",
-  },
-  {
-    id: 3,
-    title: "CorridaAttiva",
-    description: "Identidade visual",
-    image: dbd,
-    link: "https://github.com/yurizzxz/tcc-daybyday",
-    filter: "Design Gráfico",
-  },
-  {
-    id: 4,
-    title: "EngaWeb",
-    description: "Identidade visual",
-    image: dbd,
-    link: "https://github.com/yurizzxz/tcc-daybyday",
-    filter: "Design Gráfico",
-  },
-];
+// URL base do Supabase Storage
+const SUPABASE_STORAGE_URL =
+  "https://hakzttyrnyjeldmcjxpw.supabase.co/storage/v1/object/public/project-images/";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
   const [isVisibleProjects, setIsVisibleProjects] = useState(false);
-  const projectsRef = useRef(null);
+  const [selectedCategory, setSelectedCategory] = useState("Todos");
+
+  const fetchProjects = async (category = "Todos") => {
+    try {
+      let q;
+      if (category !== "Todos") {
+        q = query(
+          collection(db, "colecao"),
+          where("categoria", "==", category)
+        );
+      } else {
+        q = collection(db, "colecao");
+      }
+
+      const querySnapshot = await getDocs(q);
+      const projectsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(projectsData);
+      setProjects(projectsData);
+    } catch (error) {
+      console.error("Erro ao buscar projetos:", error);
+    }
+  };
 
   useEffect(() => {
+    fetchProjects();
+
     const observerProjects = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
@@ -57,70 +54,89 @@ const Projects = () => {
       }
     );
 
-    if (projectsRef.current) {
-      observerProjects.observe(projectsRef.current);
-    }
+    observerProjects.observe(document.querySelector("#projects"));
 
-    return () => {
-      if (projectsRef.current) {
-        observerProjects.unobserve(projectsRef.current);
-      }
-    };
+    return () => observerProjects.disconnect();
   }, []);
 
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+    fetchProjects(category);
+  };
+
   return (
-    <>
-      <main className="projects-section" id="projects" ref={projectsRef}>
-        <section className="projects">
-          <motion.div
-            initial={{ opacity: 0, y: -50, scale: 0.8 }}
-            animate={isVisibleProjects ? { opacity: 1, y: 0, scale: 1 } : {}}
-            transition={{
-              duration: 1,
-            }}
-            className="projects-header"
-          >
-            <h1 className="projects-title">Meus Projetos</h1>
-            <div className="picks">
-              <button className="pickFilter active">Todos</button>
-              <button className="pickFilter">Front-End</button>
-              <button className="pickFilter">Back-End</button>
-              <button className="pickFilter">Design Gráfico</button>
-            </div>
-          </motion.div>
-          <div className="projects-cards">
-            {projects.map((project, index) => (
+    <main className="projects-section" id="projects">
+      <section className="projects">
+        <motion.div
+          initial={{ opacity: 0, y: -50, scale: 0.8 }}
+          animate={isVisibleProjects ? { opacity: 1, y: 0, scale: 1 } : {}}
+          transition={{ duration: 1 }}
+          className="projects-header"
+        >
+          <h1 className="projects-title">Meus Projetos</h1>
+          <div className="picks">
+            <button
+              className={`pickFilter ${
+                selectedCategory === "Todos" ? "active" : ""
+              }`}
+              onClick={() => handleCategoryChange("Todos")}
+            >
+              Todos
+            </button>
+            <button
+              className={`pickFilter ${
+                selectedCategory === "frontend" ? "active" : ""
+              }`}
+              onClick={() => handleCategoryChange("frontend")}
+            >
+              Front-End
+            </button>
+            <button
+              className={`pickFilter ${
+                selectedCategory === "backend" ? "active" : ""
+              }`}
+              onClick={() => handleCategoryChange("backend")}
+            >
+              Back-End
+            </button>
+          </div>
+        </motion.div>
+
+        <div className="projects-cards">
+          {projects.length > 0 ? (
+            projects.map((project, index) => (
               <motion.div
+                key={project.id}
                 initial={{ opacity: 0, y: -50 }}
                 animate={isVisibleProjects ? { opacity: 1, y: 0 } : {}}
-                transition={{
-                  duration: 1,
-                  delay: index * 0.3,
-                }}
-                key={project.id}
+                transition={{ duration: 1, delay: index * 0.3 }}
                 className="projects-card"
               >
                 <div className="card-icon">
                   <Image
-                    src={project.image}
-                    alt={project.title}
+                    src={project.imagemUrl ? project.imagemUrl : '/default-image.jpg'}
+                    alt={project.titulo}
                     className="image-card"
                     width={510}
-                    height={50}
+                    height={0}
                     priority
+                    onLoadingComplete={() =>
+                      console.log(
+                        `Imagem carregada: ${
+                          project.imagemUrl
+                            ? `${SUPABASE_STORAGE_URL}${project.imagemUrl}`
+                            : "/default-image.jpg"
+                        }`
+                      )
+                    }
                   />
                 </div>
                 <div className="card-infos">
-                  <h1 className="title-card">{project.title}</h1>
-                  <p>{project.description}</p>
+                  <h1 className="title-card">{project.titulo}</h1>
+                  <p>{project.descricao}</p>
                   <motion.a
-                    whileHover={{
-                      y: -2,
-                      scale: 1.03,
-                    }}
-                    transition={{
-                      duration: 0.1,
-                    }}
+                    whileHover={{ y: -2, scale: 1.03 }}
+                    transition={{ duration: 0.1 }}
                     href={project.link}
                     className="greenButton projectCta"
                     target="_blank"
@@ -130,31 +146,29 @@ const Projects = () => {
                   </motion.a>
                 </div>
               </motion.div>
-            ))}
-          </div>
-          <motion.div
-            whileHover={{
-              y: -2,
-              scale: 1.03,
-            }}
-            transition={{
-              duration: 1,
-            }}
-            initial={{ opacity: 0, y: -100 }}
-            animate={isVisibleProjects ? { opacity: 1, y: 0 } : {}}
-            className="projectsCta"
+            ))
+          ) : (
+            <p style={ { marginBottom: 30 }}>Carregando projetos... Aguarde.</p>
+          )}
+        </div>
+
+        <motion.div
+          whileHover={{ y: -2, scale: 1.03 }}
+          transition={{ duration: 1 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={isVisibleProjects ? { opacity: 1, y: 0 } : {}}
+          className="projectsCta"
+        >
+          <a
+            href="https://github.com/yurizzxz?tab=repositories"
+            target="_blank"
+            className="greenButton"
           >
-            <a
-              href="https://github.com/yurizzxz?tab=repositories"
-              target="_blank"
-              className="greenButton"
-            >
-              Acesse meu GitHub
-            </a>
-          </motion.div>
-        </section>
-      </main>
-    </>
+            Acesse meu GitHub
+          </a>
+        </motion.div>
+      </section>
+    </main>
   );
 };
 
