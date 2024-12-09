664fc6700b2b692b24b2ba09882550e00d242ed7
yurizzxz <yuri.alves.silva@gmail.com>
2024-12-09 14:05:16 -0300

    Correção de pendencias

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_DOMAIN) {
+  throw new Error('NEXT_PUBLIC_SUPABASE_DOMAIN is not defined');
+}
+
 const nextConfig: NextConfig = {
   images: {
     domains: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN],
