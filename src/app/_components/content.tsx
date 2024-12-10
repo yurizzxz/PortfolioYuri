68483f74d3fbd27fd8b493e85413805ebe1f5d4c
yurizzxz <yuri.alves.silva@gmail.com>
2024-12-10 17:38:53 -0300

    style(card): adicionar efeito de hover nas bordas do card

--- src/app/_components/content.tsx
+++ src/app/_components/content.tsx
@@ -22,9 +22,9 @@ const services = [
   },
   {
     id: 3,
-    title: "Design para Sites",
+    title: "UI Design",
     description:
-      "Desenvolvo designs profissionais e atrativos que tornam a navegação intuitiva e engajante.",
+      "Desenvolvo designs atrativos que tornam a navegação intuitiva e engajante.",
     icon: "fas fa-pencil-ruler",
   },
 ];
@@ -95,7 +95,7 @@ const Content = () => {
             transition={{ duration: 1 }}
             className="services-header"
           >
-            <h1 className="services-title">Meus serviços</h1>
+            <h1 className="services-title">Serviços</h1>
             <p className="services-desc">
               Descubra como posso ajudar a transformar sua marca e presença
               digital com serviços sob medida para suas necessidades!
