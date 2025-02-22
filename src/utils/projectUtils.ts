e00e7f67bc54a68a08940ec363bc8534c109eca2
yurizzxz <yuri.alves.silva@gmail.com>
2025-02-21 21:26:24 -0300

    fix: fragmentação (hooks)  do projects/page, adição de hooks e utils

--- src/utils/projectUtils.ts
+++ src/utils/projectUtils.ts
@@ -1,3 +1,10 @@
-export const getLinguagens = (project: any) => {
-    return [project.linguagem1, project.linguagem2, project.linguagem3, project.linguagem4];
-  };
+interface Project {
+  linguagem1?: string;
+  linguagem2?: string;
+  linguagem3?: string;
+  linguagem4?: string;
+}
+
+export const getLinguagens = (project: Project) => {
+  return [project.linguagem1, project.linguagem2, project.linguagem3, project.linguagem4];
+};
