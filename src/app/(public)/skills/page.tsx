cc61d1a68f92513f00323533f85dbc408bb8c871
yurizzxz <yuri.alves.silva@gmail.com>
2025-04-25 11:07:49 -0300

    feat: add descriçao para skills

--- src/app/(public)/skills/page.tsx
+++ src/app/(public)/skills/page.tsx
@@ -2,9 +2,10 @@ import StacksIcons from "@/components/stacks-icons";
 
 export default function Skills() {
   return (
-    <section className="flex flex-col px-4 md:px-0 items-center py-32">
-      <div className="mb-10">
-        <h1 className="text-4xl font-bold">Minhas Skills</h1>
+    <section className="flex flex-col px-4 md:px-0 items-center py-28">
+      <div className="mb-5 flex items-center flex-col">
+        <h1 className="text-4xl font-bold mb-2">Minhas Skills</h1>
+        <p>Algumas das principais tecnologias e ferramentas que utilizo no meu trabalho como desenvolvedor.</p>
       </div>
       <StacksIcons />
     </section>
