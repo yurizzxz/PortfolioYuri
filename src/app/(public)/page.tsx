9bdb3a8c39e5f40931672a73a9e3afb1e89c690c
yurizzxz <yuri.alves.silva@gmail.com>
2025-06-20 13:46:30 -0300

    feat(projects): permite limitar projetos visíveis via prop e redirecionar com botão 'Ver Todos'

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/ui/separator";
 import AboutSection from "./about/page";
 import Contact from "./contact/page";
 import Hero from "./home/page";
-import Projects from "./projects/page";
+import Projects from "@/components/pages/page";
 import ServicesSection from "./works/page";
 import Skills from "./skills/page";
 import Feedbacks from "./feedbacks/page";
@@ -18,7 +18,7 @@ export default function Home() {
       <Separator />
       <ServicesSection />
       <Separator />
-      <Projects />
+      <Projects maxProjects={3} showLoadMore />
       <Separator />
       <Feedbacks />
       <Separator />
