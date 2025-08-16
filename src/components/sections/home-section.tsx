cf9c3cb424236d32b5824fcd789089103b7de39f
yurizzxz <yuri.alves.silva@gmail.com>
2025-08-16 14:34:20 -0300

    fix(layout): corrigindo espaçamentos entre conteudo do herosection

--- src/components/sections/home-section.tsx
+++ src/components/sections/home-section.tsx
@@ -26,50 +26,69 @@ const Hero = () => {
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8 }}
           >
-            <header>
+            <header >
               <motion.h1
                 className="max-[395px]:text-lg text-xl mb-0 md:mb-4"
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.8 }}
               >
-                <span className="bg-purplegradient bg-clip-text text-transparent">Olá!</span> Meu nome é
+                <span className="bg-purple-500 bg-clip-text text-transparent">
+                  Olá!
+                </span>{" "}
+                Meu nome é
               </motion.h1>
               <motion.h2
-                className="max-[395px]:text-6xl max-[340px]:text-[3.5rem] text-7xl font-bold my-[1rem]"
+                className="max-[395px]:text-6xl max-[340px]:text-[3.5rem] text-7xl font-bold"
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.8 }}
               >
-                Yuri <span className="bg-purplegradient bg-clip-text text-transparent">Alves</span>
+                Yuri{" "}
+                <span className="bg-purplegradient bg-clip-text text-transparent">
+                  Alves
+                </span>
               </motion.h2>
             </header>
 
             <motion.p
-              className="text-base mb-[2rem] max-[600px]:text-sm max-[568px]:text-base max-[425px]:text-[0.952rem] max-[360px]:text-[0.952rem] max-[340px]:text-[0.92rem]"
+              className="text-base my-4 leading-relaxed mb-6 max-[600px]:text-sm max-[568px]:text-base max-[425px]:text-[0.952rem] max-[360px]:text-[0.952rem] max-[340px]:text-[0.92rem]"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.8 }}
             >
-              Sou um <span className="bg-purplegradient bg-clip-text text-transparent">Desenvolvedor Front-End</span>{" "}
-              e <span className="bg-purplegradient bg-clip-text text-transparent">Designer Gráfico</span> apaixonado
-              pela tecnologia. Tenho 2 anos de experiência em Desenvolvimento Web e sempre estou em busca de aprender cada vez
+              Sou um{" "}
+              <span className="bg-purple-500 font-semibold bg-clip-text text-transparent">
+                Desenvolvedor Front-End
+              </span>{" "}
+              e{" "}
+              <span className="bg-purple-500 font-semibold bg-clip-text text-transparent">
+                Designer Gráfico
+              </span>{" "}
+              apaixonado pela tecnologia. Tenho 2 anos de experiência em
+              Desenvolvimento Web e sempre estou em busca de aprender cada vez
             </motion.p>
 
             <motion.div
-              className="flex flex-col gap-[2rem] mb-[1.7rem] text-[20px]"
+              className="flex flex-col gap-5 mb-3 text-[20px]"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.8 }}
             >
               <div className="flex flex-row items-center gap-[15px]">
-                <CheckCircle aria-hidden="true" className="text-[var(--spancolor)]" />
+                <CheckCircle
+                  aria-hidden="true"
+                  className="text-[var(--spancolor)]"
+                />
                 <p className="text-base max-[360px]:text-[0.9rem] max-[340px]:text-[0.92rem]">
                   Técnico em Desenvolvimento de Sistemas na ETEC Itu.
                 </p>
               </div>
               <div className="flex flex-row items-center gap-[15px]">
-                <CheckCircle aria-hidden="true" className="text-[var(--spancolor)]" />
+                <CheckCircle
+                  aria-hidden="true"
+                  className="text-[var(--spancolor)]"
+                />
                 <p className="text-base max-[360px]:text-[0.9rem] max-[340px]:text-[0.92rem]">
                   Cursando Análise e Desenvolvimento de Sistemas na Fatec Itu.
                 </p>
@@ -79,7 +98,7 @@ const Hero = () => {
             <div className="max-[330px]:flex-col  flex gap-[20px] mt-[20px] max-[568px]:my-[1rem] max-[568px]:mb-[0.7rem] max-[425px]:gap-3 max-[425px]:my-[1rem] max-[425px]:mb-[0.7rem] max-[360px]:gap-3 max-[360px]:my-[1rem] max-[360px]:mb-[0.7rem]">
               <RedirectButton
                 href="#contact"
-                className="gap-2.5 max-[330px]:mb-3"
+                className="gap-3 text-md max-[330px]:mb-3"
                 aria-label="Ir para a seção contato"
               >
                 Vamos Conversar
